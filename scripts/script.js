5eed54481bebcd6d9203038d76917f8997234511
Rustam <monk3y228@gmail.com>
2019-04-13 23:17:46 +0300

    Это наш первый коммит

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(3/0);
+
+let person = {
+    name: "Rustam",
+    age: "20",
+    isMarried: "false",
+};
+
+// let answer = prompt ("how old are u ?", "Yes");
+
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
