8440d915e828003535f105f760eb3a60e0697f35
Rustam <monk3y228@gmail.com>
2019-04-17 14:13:51 +0300

    added my homework in my repositories

---
+++ 1homework JS/scripts/script.js
@@ -0,0 +1,41 @@
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+		
+	if ( (typeof(a)) === "string" && typeof(a) != null && typeof(b) != null 
+		&& a != '' && b != '' && a.length < 20) {
+		console.log("done");
+		appData.expenses[a] = b;
+	} else {
+		console.log("bad result");
+		i--;
+	}
+	
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневый бюжет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+	console.log("Minimum");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("middle");
+} else if (appData.moneyPerDay > 2000) {
+	console.log("high");
+} else {
+	console.log("smth gone wrong")
+};
